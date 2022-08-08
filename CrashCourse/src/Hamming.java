7d2b0fdc5be8863cc065fc78f4e16e830653a667
Ivan <ivan4o200@gmail.com>
2022-08-08 23:50:32 +0300

    s

--- CrashCourse/src/Hamming.java
+++ CrashCourse/src/Hamming.java
@@ -1,17 +1,34 @@
-/*
-public class Hamming {
-    public String Hamming(String leftStrand, String rightStrand) {
+import java.util.stream.IntStream;
 
-        if (leftStrand.length() == 0 || rightStrand.length() == 0 || leftStrand.length() != rightStrand.length()){
-            return "Problem";
+public class Hamming {
+    private String leftStrand = "";
+    private String rightStrand = "";
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if(leftStrand.length() != rightStrand.length()) {
+            if(leftStrand.length() == 0) {
+                throw new IllegalArgumentException("left strand must not be empty.");
+            }
+            if(rightStrand.length() == 0) {
+                throw new IllegalArgumentException("right strand must not be empty.");
+            }
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         }
 
-        return "";
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
         //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
     public int getHammingDistance() {
+        int res = 0;
+        res = (int) IntStream.range(0, leftStrand.length()).filter(i -> leftStrand.charAt(i) != rightStrand.charAt(i)).count();
+        return res;
         //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
+
+    public static void main(String[] args) {
+        Hamming hamming = new Hamming("GG", "G");
+        System.out.println(hamming.getHammingDistance());
+    }
 }
-*/
