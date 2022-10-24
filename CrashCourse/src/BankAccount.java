97313e45b6e81e56aa9e4184b10748e95e07ecd8
Ivan <ivan4o200@gmail.com>
2022-10-24 13:57:38 +0300

    s

--- CrashCourse/src/BankAccount.java
+++ CrashCourse/src/BankAccount.java
@@ -1,4 +1,50 @@
 public class BankAccount {
-    //another test
-    //test
+
+
+    private int balance = 0;
+    private boolean isOpen = false;
+
+    void open() {
+
+        this.isOpen = true;
+        BankAccount bankAccount = new BankAccount();
+    }
+
+
+    synchronized void deposit(Integer amount) {
+
+        if (this.isOpen) {
+            balance += amount;
+        }
+    }
+
+    synchronized void withdraw(Integer amount) {
+        int onlyIfBalanceNotNegative = balance - amount;
+        if (this.isOpen && onlyIfBalanceNotNegative >= 0) {
+            balance -= amount;
+        }
+    }
+
+    public synchronized int getBalance() {
+        if (this.isOpen) {
+            System.out.println(balance);
+            return balance;
+        }
+
+        return 0;
+    }
+
+    void close() {
+        this.isOpen = false;
+    }
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.open();
+        bankAccount.deposit(10);
+        bankAccount.withdraw(15);
+        bankAccount.getBalance();
+
+    }
 }
